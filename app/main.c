a70772703761cde5f5c3a58a9a984989970fed39
Sai Hari Chandana Kalluri <chandana.kalluri@xilinx.com>
2021-05-13 22:52:19 -0700

    main.c: Remove interval support
    Current interval implementation is not similar to watch command.
    Disable this feature for now.
    
    Fix file indentations.
    
    Signed-off-by: Sai Hari Chandana Kalluri <chandana.kalluri@xilinx.com>

--- app/main.c
+++ app/main.c
@@ -1,26 +1,26 @@
 /******************************************************************************
-*
-* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMANGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-******************************************************************************/
+ *
+ * Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMANGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ ******************************************************************************/
 
 /***************************** Include Files *********************************/
 #include <stdio.h>
@@ -36,28 +36,26 @@
 
 /************************** Variable Definitions *****************************/
 static int verbose_flag=0;
-int interval=1;
 char *filename;
 
 /************************** Function Definitions *****************************/
 /*****************************************************************************/
 /**
-*
-* This function prints command usage for platformstats utility.
-*
-* @param    None
-*
-* @return   None
-*
-* @note     None
-*
-*******************************************************************************/
+ *
+ * This function prints command usage for platformstats utility.
+ *
+ * @param    None
+ *
+ * @return   None
+ *
+ * @note     None
+ *
+ *******************************************************************************/
 static void print_usage()
 {
 	printf(" XILINX PLATFORM STATS UTILITY \n");
 	printf(" Usage: platformstats [options] [stats]\n\n");
 	printf(" Options \n");
-	printf(" 	-i --interval		Specify the decimal value for polling in ms. The default is 1000ms.  \n");
 	printf(" 	-v --verbose		Print verbose messages  \n");
 	printf(" 	-l --logfile		Print output to logfile  \n");
 	printf(" 	-s --stop   		Stop any running instances of platformstats  \n");
@@ -81,7 +79,6 @@ int main(int argc, char *argv[])
 		{"brief", no_argument, &verbose_flag, 0},
 		{"all", no_argument, 0, 'a'},
 		/* These options dont set a flag; */
-		{"interval", required_argument, 0, 'i'},
 		{"logfile", required_argument, 0, 'l'},
 		{"stop", required_argument, 0, 's'},
 		{"help", no_argument, 0, 'h'},
@@ -93,21 +90,18 @@ int main(int argc, char *argv[])
 	};
 
 	/* Parse arguments */
-	opt = getopt_long(argc, argv, "voacpmfi:l:s:h",long_options, &options_index);
+	opt = getopt_long(argc, argv, "voacpmf:l:s:h",long_options, &options_index);
 	switch(opt)
 	{
 		case 'v':
 			verbose_flag = 1;
 			break;
-		case 'i':
-			interval = atoi(optarg);
-			break;
 		case 'l':
 			filename = optarg;
 			if(filename)
 			{
-			        int fd = open(filename,O_CREAT |O_WRONLY | O_APPEND, 0755);
-			        dup2(fd,1);
+				int fd = open(filename,O_CREAT |O_WRONLY | O_APPEND, 0755);
+				dup2(fd,1);
 			}
 			break;
 		case 'h':
@@ -115,47 +109,20 @@ int main(int argc, char *argv[])
 			break;
 		case 'a':
 			print_all_stats(verbose_flag);
-			for(int i=1; i<interval; i++)
-			{
-				sleep(1);
-				print_all_stats(verbose_flag);
-			}
 			break;
 		case 'c':
 			print_cpu_utilization(verbose_flag);
-			for(int i=1; i<interval; i++)
-			{
-				sleep(1);
-				print_cpu_utilization(verbose_flag);
-			}
 			break;
 		case 'p':
 			print_power_utilization(verbose_flag);
-			for(int i=1; i<interval; i++)
-			{
-				sleep(1);
-				print_power_utilization(verbose_flag);
-			}
 			break;
 		case 'm':
 			print_cma_utilization(verbose_flag);
 			print_swap_memory_utilization(verbose_flag);
 			print_ram_memory_utilization(verbose_flag);
-			for(int i=1; i<interval; i++)
-			{
-				sleep(1);
-				print_cma_utilization(verbose_flag);
-				print_swap_memory_utilization(verbose_flag);
-				print_ram_memory_utilization(verbose_flag);
-			}
 			break;
 		case 'f':
 			print_cpu_frequency(verbose_flag);
-			for(int i=1; i<interval; i++)
-			{
-				sleep(1);
-				print_cpu_frequency(verbose_flag);
-			}
 			break;
 		default:
 			print_all_stats(verbose_flag);
